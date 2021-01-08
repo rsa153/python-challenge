d1ee9425dcac81c29ba27970f0aef4c06e1a8983
rsa153 <r-saber@nlaw.northwestern.edu>
2021-01-07 18:48:41 -0600

    PyPoll set variables

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,13 @@ import csv
 # Set path for file
 budget_data = os.path.join("Resources", "election_data.csv")
 
+# Set variables
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
 # Open and read csv
 with open(budget_data, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
